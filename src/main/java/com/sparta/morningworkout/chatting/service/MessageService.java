a720184f3c98e980c8bbc11c1b06508db630c44f
Seowoo <5w31892p@gmail.com>
2023-01-30 20:55:50 +0900

    <fix> MessageService parameter fix

--- src/main/java/com/sparta/morningworkout/chatting/service/MessageService.java
+++ src/main/java/com/sparta/morningworkout/chatting/service/MessageService.java
@@ -31,17 +31,10 @@ public class MessageService {
 			() -> new IllegalArgumentException("채팅방이 존재하지 않습니다.")
 		);
 
-		// Profile profile = profileRepository.findById().orElseThrow(
-		// 	() -> new IllegalArgumentException("보내는 사용자가 올바르지 않습니다.")
-		// );
-		// if (rooms.getSellerId() == profile.getId()) {
-		// 	messageRepository.save(profile.getNickname())
-		// }
-
 		if (message.getProductId() != rooms.getProductId()) {
 			throw new IllegalArgumentException("해당 상품의 채팅방이 존재하지 않습니다.");
 		} else {
-			Message messages = new Message(message.getSender(), message.getReceiver(), message.getMessage(), rooms);
+			Message messages = new Message(message.getMessage(), rooms);
 			messageRepository.save(messages);
 
 			return messages;
